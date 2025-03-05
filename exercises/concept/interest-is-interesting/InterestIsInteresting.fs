003bca76f0b31741dca8a6316a0606db99833d5d
Radhi <153525225+Bajahaw@users.noreply.github.com>
2025-03-05 03:17:42 +0100

    InterestIsInteresting solved!

--- exercises/concept/interest-is-interesting/InterestIsInteresting.fs
+++ exercises/concept/interest-is-interesting/InterestIsInteresting.fs
@@ -1,13 +1,19 @@
 module InterestIsInteresting
 
 let interestRate (balance: decimal): single =
-    failwith "Please implement the 'interestRate' function"
-
+   match balance with
+   | b when b < 0m -> 3.213f
+   | b when b < 1000m -> 0.5f
+   | b when 5000m > b && b >= 1000m -> 1.621f
+   | _ -> 2.475f 
+   
 let interest (balance: decimal): decimal =
-   failwith "Please implement the 'interest' function"
+   balance * (decimal (interestRate balance) / 100m)
 
 let annualBalanceUpdate(balance: decimal): decimal =
-   failwith "Please implement the 'annualBalanceUpdate' function"
+   balance + balance * decimal (interestRate balance / 100f)
 
 let amountToDonate(balance: decimal) (taxFreePercentage: float): int =
-   failwith "Please implement the 'amountToDonate' function"
+   if balance > 0m then
+      int (balance * decimal (taxFreePercentage / 100.0 * 2.0))
+   else 0
