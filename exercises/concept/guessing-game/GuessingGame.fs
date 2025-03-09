7d4cef430c9497a3ad9532aba03d9c3cd0ff552e
Radhi <153525225+Bajahaw@users.noreply.github.com>
2025-03-09 23:46:11 +0100

    GuessingGame solved!

--- exercises/concept/guessing-game/GuessingGame.fs
+++ exercises/concept/guessing-game/GuessingGame.fs
@@ -1,3 +1,10 @@
 module GuessingGame
 
-let reply (guess: int): string = failwith "Please implement the 'reply' function"
+let reply (guess: int): string =
+    match guess - 42 with
+    | 1 -> "So close"
+    | -1 -> "So close"
+    | b when b > 1 -> "Too high"
+    | b when b < -1 -> "Too low"
+    | _ -> "Correct"
+    
