0b12d78bb50c598ca00852913018ba35a76267c5
Radhi <153525225+Bajahaw@users.noreply.github.com>
2025-03-12 23:28:03 +0100

    SqueakyClean sloved!

--- exercises/concept/squeaky-clean/SqueakyClean.fs
+++ exercises/concept/squeaky-clean/SqueakyClean.fs
@@ -3,7 +3,13 @@ module SqueakyClean
 open System
 
 let transform (c: char) : string =
-    failwith "Please implement the 'transform' function"
+    match c with
+    | '-' -> "_"
+    | ' ' -> ""
+    | c when Char.IsUpper c -> "-" + Char.ToString (Char.ToLower c) 
+    | c when Char.IsDigit c -> ""
+    | c when int c >= int 'α' && int c <= int 'ω' -> "?"
+    | _ -> Char.ToString c
     
 let clean (identifier: string): string =
-    failwith "Please implement the 'clean' function"
+    String.collect transform identifier
