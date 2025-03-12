fe62a91e8a85cc92ba48f69ee97a22809342ba5d
Radhi <153525225+Bajahaw@users.noreply.github.com>
2025-03-12 03:07:31 +0100

    TisburyTreasureHunt solved!

--- exercises/concept/tisbury-treasure-hunt/TisburyTreasureHunt.fs
+++ exercises/concept/tisbury-treasure-hunt/TisburyTreasureHunt.fs
@@ -1,13 +1,22 @@
 module TisburyTreasureHunt
 
+open System
+open FsUnit.Xunit
+
 let getCoordinate (line: string * string): string =
-    failwith "Please implement the 'getCoordinate' function"
+    snd line
 
 let convertCoordinate (coordinate: string): int * char = 
-    failwith "Please implement the 'convertCoordinate' function"
+    Int32.Parse(string coordinate[0]), coordinate[1]
+
+let mid (_,c,_) = c
 
 let compareRecords (azarasData: string * string) (ruisData: string * (int * char) * string) : bool = 
-    failwith "Please implement the 'compareRecords' function"
+    convertCoordinate (snd azarasData) = mid ruisData
 
 let createRecord (azarasData: string * string) (ruisData: string * (int * char) * string) : (string * string * string * string) =
-    failwith "Please implement the 'createRecord' function"
+    let (a, _, c) = ruisData
+    let (d, e) = azarasData
+    match e with
+    | e when convertCoordinate e = mid ruisData -> (e , a, c, d)
+    | _ -> ("", "", "", "")
